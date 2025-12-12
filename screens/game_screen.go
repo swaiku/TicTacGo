87abdd576c11c4c266accebc5221def6777491b0
Alexandre Schmid <alexandre.schmid@edu.hefr.ch>
2025-12-12 11:36:47 +0100

    - Add unit tests for core game logic (board, game flow, reset, win/draw) - Add tests for AI models (RandomAI, MinimaxAI) - Introduce fuzz testing to ensure Minimax robustness - Improve overall code coverage to ~86% - Document test intent for clarity and maintainability

--- screens/game_screen.go
+++ screens/game_screen.go
@@ -133,7 +133,16 @@ func (gs *GameScreen) Draw(screen *ebiten.Image) {
 func (gs *GameScreen) drawEndMessage(screen *ebiten.Image) {
 	var msg string
 	if gs.game.Winner != nil {
-		msg = fmt.Sprintf("%s wins!", gs.game.Winner.Symbol.Type)
+		var sym string
+		switch gs.game.Winner.Symbol.Type {
+		case game.CROSS:
+			sym = "X"
+		case game.CIRCLE:
+			sym = "O"
+		}
+
+		msg = fmt.Sprintf("%s wins!", sym)
+
 	} else {
 		msg = "It's a draw!"
 	}
